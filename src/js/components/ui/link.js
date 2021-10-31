4b99bd8c786dbf9243b9560462417b90ad0896a8
SeonhyeYoon <ysunh118@gmail.com>
2021-10-31 14:26:32 -0600

    correct error-4

--- src/js/components/ui/link.js
+++ src/js/components/ui/link.js
@@ -2,7 +2,7 @@ import makeElement from "../../utils/makeElement";
 
 
 const link = function (label="ui link", path="/", className="ui-link"){
-     const template = `<a href="${path}" class="${className}" data-path="${path}" route="${path}" >${label}</a>`
+     const template = `<a href="${path}" class="${className}" data-path="${path}" >${label}</a>`
      const element =  makeElement(template)
      return element
 }
--- src/js/index.js
+++ src/js/index.js
@@ -12,7 +12,7 @@ const onAppInit = async function(e){
     }
 
     createStore(todoItmes);
-    Router(window.location.pathname);
+    Router('./pages/todo.js');
 }
 
 window.addEventListener('load', onAppInit)
